6a4ad51b9c04604dbf5b2019a76be94a9fb80b96
Joberth <joberthdavid@hotmail.com>
2020-11-07 19:06:42 -0300

    Movimentação dos componentes para a pasta app/componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao/barra-navegacao.component';
-import { CabecalhoFeedsComponent } from './cabecalho-feeds/cabecalho-feeds/cabecalho-feeds.component';
-import { PostFeedsComponent } from './post-feeds/post-feeds/post-feeds.component';
-import { MenuSugestoesComponent } from './menu-sugestoes/menu-sugestoes/menu-sugestoes.component';
+import { BarraNavegacaoComponent } from './componentes/barra-navegacao/barra-navegacao.component';
+import { CabecalhoFeedsComponent } from './componentes/cabecalho-feeds/cabecalho-feeds.component';
+import { PostFeedsComponent } from './componentes/post-feeds/post-feeds.component';
+import { MenuSugestoesComponent } from './componentes/menu-sugestoes/menu-sugestoes.component';
 
 @NgModule({
   declarations: [
